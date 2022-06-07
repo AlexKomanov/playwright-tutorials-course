64c5b15b5cd689c8bf60edf81fc7ca904b9702fa
AlexKomanov <akomanov88@gmail.com>
2022-06-07 06:25:02 +0300

    Changes in the submenu-styling.js

--- src/components/submenu/submenu-styling.js
+++ src/components/submenu/submenu-styling.js
@@ -4,7 +4,22 @@ import styled from "styled-components";
 export const SidebarLink = styled(Link)`
     display: flex;
     color: #e1e9fc;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+    list-style: none;
+    height: 60px;
+    text-decoration: none;
+    font-size: 18px;
+
+    &:hover {
+        background: #252831;
+        border-left: 3px solid gold;
+        cursor: pointer;
+    }
 `;
 
 
-export const SidebarLabel = styled.span``;
+export const SidebarLabel = styled.span`
+    margin-left: 16px;
+`;
